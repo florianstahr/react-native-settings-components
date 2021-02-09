9b826a454ad333a95ac6703823147e90f1b8ac8a
igoro00 <igor.ordecha@gmail.com>
2021-02-09 18:44:20 +0100

    added SettingsEditText typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ViewStyle } from 'react-native'
+import { TextProps, TextStyle, TouchableOpacityProps, ViewProps, ViewStyle } from 'react-native'
 
 interface SettingsDividerShortProps {
 	ios?:boolean,
@@ -15,3 +15,53 @@ interface SettingsDividerLongProps {
 	dividerStyle?:ViewStyle,
 }
 type SettingsDividerLong = React.Component<SettingsDividerLongProps>;
+
+interface SettingsEditTextProps {
+	containerProps?: ViewProps,
+	containerStyle?: ViewStyle,
+	disabledOverlayStyle?: ViewStyle,
+	titleProps?: TextProps,
+	titleStyle?: TextStyle,
+	title: string,
+	valueProps?: TextProps,
+	valueStyle?: TextStyle,
+	value: string
+	valuePlaceholder?: string, 
+	valueFormat?: (input:string)=>string ,
+	negativeButtonTitle: string,
+	positiveButtonTitle: string,
+	dialogDescription?: string,
+	onValueChange: (text:string)=>void,
+	disabled?: boolean,
+	iosDialogInputType?:( 
+		'plain-text'|
+		'plain-text'|
+		'email-address'|
+		'numeric'|
+		'phone-pad'|
+		'ascii-capable'|
+		'numbers-and-punctuation'|
+		'url'|
+		'number-pad'|
+		'name-phone-pad'|
+		'decimal-pad'|
+		'twitter'|
+		'web-search'
+	),
+	androidDialogInputType?: (
+		null|
+		'numeric'|
+		'numbers-and-punctuation'|
+		'numeric-password'|
+		'email-address'|
+		'password'|
+		'phone-pad'|
+		'decimal-pad'
+	),
+	androidDialogPlaceholder?:string,
+	// ↓↓↓ it's from react-native-dialogs package -> DialogAndroid.js -> "type OptionsPrompt" (on 183 line currently)
+	// ↓↓↓ I just don't know how to add it properly
+	androidDialogOptions?: {any:any},
+	touchableProps?: TouchableOpacityProps
+}
+type SettingsEditText = React.Component<SettingsEditTextProps>;
